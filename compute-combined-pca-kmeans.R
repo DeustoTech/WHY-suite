1ca9f58ce9b26b607e88faeed4944dfd355c1bb3
Carlos Quesada Granja <49593121+quesadagranja@users.noreply.github.com>
2020-09-17 18:30:33 +0200

    review of compute-combined-pca-kmeans

--- compute-combined-pca-kmeans.R
+++ compute-combined-pca-kmeans.R
@@ -1,15 +1,21 @@
 # Carlos Quesada - Universidad de Deusto
 # 2020.09.03
-# Compute PCA from a CSV file of features
+# Compute a combination of PCA and k-means from a CSV file of features
 
 # Load source file and libraries
-source("why-source_v04.R")
+source("why-source.R")
 # Cluster-PCA visualization
 library(factoextra)
 
-# Dataset key
+################################################################################
+# -- User parameters
+################################################################################
+
+# Code of the dataset
 dset_key <- "lcl"
-feats_folder <- "2013 Feb, 0% NA, scale=FALSE, 70 feats/"
+# Dataset key
+root_folder <- "G:/Mi unidad/WHY/Resultados/lcl/features/"
+feats_subfolder <- "2013 Feb, 0% NA, scale=FALSE, 70 feats/"
 
 # Compute only PCA?
 only_pca <- F
@@ -20,7 +26,7 @@ pca_components <- 4
 elbow_method <- F
 max_clusters <- 20
 # In case elbow_method is FALSE
-number_of_centers <- 8
+number_of_centers <- 4
 
 # Observations to plot
 otp <- 1:4605
@@ -45,25 +51,25 @@ ftp <- c(5:9, 21:22)
 axis_x <- 1
 axis_y <- 2
 
+# Plot accumulated months of each cluster?
+month_accumul <- FALSE
+
+################################################################################
+
 # Path to features
-feats_subfolder <- paste(
-  "G:/Mi unidad/WHY/Resultados/", 
-  dset_key,
-  "/features/",
-  feats_folder,
-  sep = ""
-)
+feats_folder <- paste(root_folder, feats_subfolder, "feats.csv", sep="")
+datainfo_folder <- paste(root_folder, feats_subfolder, "data_info.csv", sep="")
 
 # Load data from CSV file
 feats <- read.table(
-  file = paste(feats_subfolder, "feats.csv", sep = ""),
+  file = feats_folder,
   header = TRUE,
   sep = ","
 )
 
 # Load data info
 data_info <- read.table(
-  file = paste(feats_subfolder, "data_info.csv", sep = ""),
+  file = datainfo_folder,
   header = TRUE,
   sep = ","
 )
@@ -119,35 +125,41 @@ if (only_pca == F) {
   }
 }
 
-# Plot clusters content ########################################################
-for (center in 1:number_of_centers) {
-  # Initialization
-  total_mean <- numeric(48)
-  # Load data
-  ts_ids <- as.numeric(names(km$cluster[km$cluster == center]))
-  ts_names <- data_info[ts_ids, 1]
-  length_ts_names <- length(ts_names)
-  
-  for (ts_name in ts_names) {
-    ts <- Load_Dataset_File(dset_key, "", ts_name)
-    print(ts_name)
-    for (day in 1:27) {
-      # Take just a day
-      ts_ival <- Get_Data_Interval(
-        ts,
-        ISOdate(2013, 2, day, 0, 0, 0),
-        ISOdate(2013, 2, day + 1, 0, 0, 0), 
-        86400 / 48
-      )
-      total_mean <- total_mean + (1/length_ts_names/28) * ts_ival
+################################################################################
+# Plot month accumulated consumption of each cluster
+# (CONSIDER MOVE TO why-source.R)
+################################################################################
+
+if (month_accumul == TRUE) {
+  for (center in 1:number_of_centers) {
+    # Initialization
+    total_mean <- numeric(48)
+    # Load data
+    ts_ids <- as.numeric(names(km$cluster[km$cluster == center]))
+    ts_names <- data_info[ts_ids, 1]
+    length_ts_names <- length(ts_names)
+    
+    for (ts_name in ts_names) {
+      ts <- Load_Dataset_File(DATASET_PATH, ts_name)
+      print(ts_name)
+      for (day in 1:27) {
+        # Take just a day
+        ts_ival <- Get_Data_Interval(
+          ts,
+          ISOdate(2013, 2, day, 0, 0, 0),
+          ISOdate(2013, 2, day + 1, 0, 0, 0), 
+          86400 / 48
+        )
+        total_mean <- total_mean + (1/length_ts_names/28) * ts_ival
+      }
     }
+    plot(seq(0,24,0.5),
+         ts_ival, 
+         type = "l", 
+         ylim=c(0,5), 
+         xlab="Hours", 
+         ylab="kWh",
+         main=paste("Cluster #", center, sep="")
+    )
   }
-  plot(seq(0,24,0.5),
-       ts_ival, 
-       type = "l", 
-       ylim=c(0,5), 
-       xlab="Hours", 
-       ylab="kWh",
-       main=paste("Cluster #", center, sep="")
-  )
 }
