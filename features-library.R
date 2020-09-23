634983da039d41032e33f0d327e3ec6264eb4829
Carlos Quesada Granja <49593121+quesadagranja@users.noreply.github.com>
2020-09-23 18:39:40 +0200

    remove useless comments

--- features-library.R
+++ features-library.R
@@ -6,14 +6,11 @@
 source("why-source.R")
 
 # User parameters
-# from_date <- ISOdate(2013, 2, 1, 0, 0, 0) #"first"
-# to_date <- ISOdate(2013, 2, 28, 23, 30, 0) #"last"
-# samples_per_day <- list(lcl = 48)
 sampling_period <- 86400 / SAMPLES_PER_DAY[["lcl"]]
 results_folder <- "G:/Mi unidad/WHY/Resultados/lcl/features/2012-2013, 0% NA, scale=FALSE, 70 feats/"
 feats_to_plot <- c(1:10, 15:70)
 
-# Call the function
+# Function call
 Create_Features_Library(
   sampling_period = sampling_period,
   results_folder  = results_folder,
