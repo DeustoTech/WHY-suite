138dedf2c867e4db10d1afb833ce2a0ca8f2bbf7
Carlos Quesada Granja <49593121+quesadagranja@users.noreply.github.com>
2020-10-05 11:55:22 +0200

    remove useless header

--- whyT2.1pkg/R/why-source.R
+++ whyT2.1pkg/R/why-source.R
@@ -2,47 +2,6 @@
 # 2020.07.03
 # Source file
 
-#-- LIBRARIES
-
-# library(tidyverse)
-# library(tsfeatures)
-# library(lubridate)
-# library(forecast)
-# library(moments)
-
-
-#-- ESTABLISH A SEED FOR THE RANDOM NUMBERS
-set.seed(1981)
-
-#-- GLOBAL VARS (IN CAPITAL LETTERS)
-
-# Path to the dataset folder
-# DATASET_PATH <- "G:/Mi unidad/WHY/Datasets/lcl/"
-
-# List of functions that will be passed to tsfeatures.
-# -- Some functions were not included because they:
-# -- TOOK A LOT OF TIME:
-# -- "compengine", "sampenc", "sampen_first", "fluctanal_prop_r1"
-# -- DIDN'T WORK:
-# -- "binarize_mean", "embed2_incircle" (require param), "hw_parameters"
-# BASIC_ANALYSIS_LIST <-
-#   c(
-#     "stat_moments", "quantiles", "electricity",
-#     "frequency" , "stl_features", "entropy", "acf_features"
-#   )
-# EXTRA_ANALYSIS_LIST <-
-#   c(
-#     "stat_moments", "quantiles", "electricity",
-#     "frequency", "stl_features", "entropy", "acf_features" , "max_kl_shift",
-#     "outlierinclude_mdrmd", "arch_stat", "max_level_shift", "ac_9",
-#     "crossing_points", "max_var_shift", "nonlinearity",
-#     "spreadrandomlocal_meantaul" , "flat_spots", "pacf_features", "firstmin_ac",
-#     "std1st_der", "heterogeneity", "stability", "firstzero_ac", "trev_num",
-#     "holt_parameters", "walker_propcross", "hurst", "unitroot_kpss",
-#     "histogram_mode", "unitroot_pp", "localsimple_taures", "lumpiness",
-#     "motiftwo_entro3"
-#   )
-
 ################################################################################
 ##  stat_moments()
 ################################################################################
