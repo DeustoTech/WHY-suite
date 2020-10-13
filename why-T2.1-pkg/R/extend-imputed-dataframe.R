85cf3007b945a1549f7a41aa9cb79516562bf8c0
Carlos Quesada Granja <49593121+quesadagranja@users.noreply.github.com>
2020-10-13 09:44:15 +0200

    new (draft)

---
+++ why-T2.1-pkg/R/extend-imputed-dataframe.R
@@ -0,0 +1,15 @@
+#' Extension of time series in imputed dataframe
+#'
+#' @description
+#' Impute missing samples in a cooked dataframe. It can use an algorithm for short gaps (e.g. "interpolation") and another one for longer gaps (e.g. "locf").
+#'
+#' @param cdf Cooked dataframe.
+#' @param season Seasonal period (e.g. 1 week) in number of samples.
+#' @param short_gap Number of samples considered as a short gap.
+#' @param short_algorithm Algorithm used to impute short gaps.
+#' @param long_algorithm Algorithm used to impute long gaps.
+#'
+#' @return Imputed dataframe, i.e. a cooked dataframe with a 3rd column indicating if each sample has been imputed or not.
+#'
+#' @export
+
