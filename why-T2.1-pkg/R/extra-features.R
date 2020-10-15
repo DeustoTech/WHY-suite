2f31904e90644fec330f8d9d1e63e0c91086b176
Carlos Quesada Granja <49593121+quesadagranja@users.noreply.github.com>
2020-10-15 16:05:46 +0200

    minor corr

--- why-T2.1-pkg/R/extra-features.R
+++ why-T2.1-pkg/R/extra-features.R
@@ -52,14 +52,14 @@ quantiles <- function(x) {
 
 electricity <- function(x) {
   # Get samples per day
-  samples_per_day <- attr(x, "msts")[1]
+  spd            <- attr(x, "msts")[1]
   # Pad with NA
-  samples_to_pad  <- samples_per_day[1] - (length(x) %% samples_per_day[1])
-  padded_x        <- c(as.numeric(x), rep(0, samples_to_pad))
-  # Convert TS to matrix of 28 days x "samples_per_day"
-  x_matrix        <- t(matrix(padded_x, nrow=samples_per_day))
+  samples_to_pad <- (spd - (length(x) %% spd)) %% spd
+  padded_x       <- c(as.numeric(x), rep(0, samples_to_pad))
+  # Convert TS to matrix of size = 28 days x "spd"
+  x_matrix       <- t(matrix(padded_x, nrow=spd))
   # List of load factors (one per day)
-  load_factors    <- rowMeans(x_matrix)/Rfast::rowMaxs(x_matrix, value=TRUE)
+  load_factors   <- rowMeans(x_matrix)/Rfast::rowMaxs(x_matrix, value=TRUE)
   # Return
   list(
     mean_load_factors = mean(load_factors),
