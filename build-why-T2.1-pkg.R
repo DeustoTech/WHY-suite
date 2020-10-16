d217028c67a4d90e584da498743bb6f8fa012bad
Carlos Quesada Granja <49593121+quesadagranja@users.noreply.github.com>
2020-10-16 14:18:44 +0200

    ggplot2 stuff

--- build-why-T2.1-pkg.R
+++ build-why-T2.1-pkg.R
@@ -1,13 +1,14 @@
 library(roxygen2)
 library(devtools)
 setwd("why-T2.1-pkg")
-roxygen2::roxygenize()
+#roxygen2::roxygenize()
 # devtools::build(
 #   binary=TRUE
 # )
 #devtools::check()
 devtools::install(
-  upgrade=FALSE
+  upgrade = FALSE,
+  quick   = TRUE
   )
 setwd("..")
 library(whyT2.1)
--- why-T2.1-pkg/R/plot_dataframe.R
+++ why-T2.1-pkg/R/plot_dataframe.R
@@ -23,24 +23,18 @@ plot_dataframe <- function(dset_data, title=NULL) {
     coloring = "blue"
   }
 
-  p <- ggplot2::ggplot(
+  p <- 
     # Data to be plotted
-    data = dset_data,
-    mapping = ggplot2::aes(x=times, y=values)
+    ggplot2::ggplot(
+      data    = dset_data,
+      mapping = ggplot2::aes(x=times, y=values)
     ) +
     # Type of graph (line in this case)
-    ggplot2::geom_line(
-      color = coloring
-    ) +
+    ggplot2::geom_line(color = coloring) +
     # Title
-    ggplot2::ggtitle(
-      title
-    ) +
+    ggplot2::ggtitle(title) +
     # Labels
-    ggplot2::labs(
-      x = "Date",
-      y = "kWh"
-    ) + 
+    ggplot2::labs(x = "Date", y = "kWh") + 
     # Axis limits
     ggplot2::scale_y_continuous(limits = c(0,5))
 
