4ee9a3e253f6bda70ca2997ea36c2d0ae8900a59
Carlos Quesada Granja <49593121+quesadagranja@users.noreply.github.com>
2021-04-15 02:42:07 +0200

    Update dataset-load-and-processing.R

--- R/dataset-load-and-processing.R
+++ R/dataset-load-and-processing.R
@@ -449,8 +449,8 @@ extend_dataset <- function(input_folder, output_folder, wanted_days, dset_key, m
   doParallel::registerDoParallel(cl)
   
   # Analysis loop
-  foreach::foreach (x = 1:length(dset_filenames)) %dopar% {
-  #for(x in 1:length(dset_filenames)) {
+  #foreach::foreach (x = 1:length(dset_filenames)) %dopar% {
+  for(x in 1:length(dset_filenames)) {
     print(dset_filenames[x])
     # File name selection
     dset_filename <- dset_filenames[x]
