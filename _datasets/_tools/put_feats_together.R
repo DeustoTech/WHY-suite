a7daca85cc33d67d8b204d3eb79d20f631050b6c
Carlos Quesada Granja <49593121+quesadagranja@users.noreply.github.com>
2021-06-01 18:08:36 +0200

    Create put_feats_together.R
    file to put conventional features and catch-22 features together

---
+++ _datasets/_tools/put_feats_together.R
@@ -0,0 +1,30 @@
+################################################################
+##  Put conventional features and catch-22 features together  ##
+################################################################
+
+# Conventional features file path
+ft_path <- "G:/Mi unidad/WHY/Features/feats_21.05.26/feats.csv"
+# Catch-22 folder
+c22_dir <- "G:/Mi unidad/WHY/Features/feats_21.05.26/"
+# Catch-22 file vector
+c22_v <- paste0(c22_dir, "c22_feats_", seq(1,23501,500), ".csv")
+
+# Read conventional feats file
+conv_feats <- data.table::fread(
+  file   = ft_path,
+  header = TRUE,
+  sep    = ","
+)
+
+# Read Catch-22 feats file
+c22_feats <- data.frame()
+for (c22_path in c22_v) {
+  c22_df <- data.table::fread(
+    file   = c22_path,
+    header = TRUE,
+    sep    = ","
+  )
+  c22_feats <- rbind(c22_feats, c22_df)
+}
+  
+browser()
