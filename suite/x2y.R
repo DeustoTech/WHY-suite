09463dc36e168bc48c36b5cf4568449e18ad1a07
Carlos Quesada <49593121+quesadagranja@users.noreply.github.com>
2022-02-17 12:25:33 +0100

    imp2fea por

--- suite/x2y.R
+++ suite/x2y.R
@@ -20,9 +20,20 @@ mdata_file_iss <- "/home/ubuntu/carlos.quesada/disk/iss/meta/iss_meta.csv"
 mdata_file_lcl <- "/home/ubuntu/carlos.quesada/disk/lcl/meta/lcl_meta.csv"
 mdata_file_por <- NULL
 
+# Feature files
+fea_file_por <- "/home/ubuntu/carlos.quesada/disk/features/por_22.02.17/feats_por.csv"
+
 ################################################################################
-operation <- 1
+operation <- 2
 ################################################################################
+
+# 2022.02.17 - imp2fea POR
+if (operation == 2) {
+  imp2fea(
+    imp_dir  = imp_dir_por,
+    fea_file = fea_file_por
+  )
+}
 
 # 2022.02.17 - raw2imp POR
 if (operation == 1) {
